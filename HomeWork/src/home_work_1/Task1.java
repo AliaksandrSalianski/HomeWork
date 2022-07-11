75274e37cbc4ab40c264ab9c634e90bbc04b670d
AliaksandrSalianski <AliaksandrSalianski@gmail.com>
2022-07-11 16:44:24 -0400

    Task1: -clean up cod

--- HomeWork/src/home_work_1/Task1.java
+++ HomeWork/src/home_work_1/Task1.java
@@ -4,13 +4,20 @@ import java.util.Scanner;
 
 public class Task1 {
     public static void doBitwiseOperations() {
+        //Bitwise operations only work on integer variables.
+        //It is not possible to perform an operation with the number 42.5
+        String error = "Sorry, entered NOT a number or a FLOATING POINT number ";
         Scanner scanner = new Scanner(System.in);
-        int numberOne;
+        int numberOne = 0;
         int numberTwo;
         System.out.println("Please, enter two numbers");
         if (scanner.hasNextInt()) {
             numberOne = scanner.nextInt();
             System.out.printf("NumberOne = %d\n", numberOne);
+        } else {
+            System.out.println(error);
+        }
+        if (scanner.hasNextInt()) {
             numberTwo = scanner.nextInt();
             System.out.printf("NumberTwo = %d\n", numberTwo);
             System.out.printf("Result of operation \"|\" = %d\n", numberOne | numberTwo);
@@ -28,9 +35,7 @@ public class Task1 {
                       00001010  10
             */
         } else {
-            //Bitwise operations only work on integer variables.
-            //It is not possible to perform an operation with the number 42.5
-            System.out.println("Sorry, entered NOT a number or a FLOATING POINT number ");
+            System.out.println(error);
         }
         scanner.close();
     }
