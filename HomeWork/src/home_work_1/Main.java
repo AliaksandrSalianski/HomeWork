ff621d98d6f09c1213d2c150e2c45725a110311f
AliaksandrSalianski <AliaksandrSalianski@gmail.com>
2022-07-11 19:01:25 -0400

    Task2 -cod cleanup

---
+++ HomeWork/src/home_work_1/Main.java
@@ -0,0 +1,12 @@
+package home_work_1;
+
+public class Main {
+    public static void main(String[] args) {
+        int a = 2;
+        int b = 8;
+        int c = 5;
+        int result;
+
+        System.out.println(true && false);
+    }
+}
