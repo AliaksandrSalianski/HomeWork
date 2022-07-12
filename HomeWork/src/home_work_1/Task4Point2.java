bb3938f777f2686a35352c89d524f918b60e9de1
AliaksandrSalianski <AliaksandrSalianski@gmail.com>
2022-07-12 12:31:19 -0400

    Task4Point2

--- HomeWork/src/home_work_1/Task4Point2.java
+++ HomeWork/src/home_work_1/Task4Point2.java
@@ -2,8 +2,8 @@ package home_work_1;
 
 public class Task4Point2 {
     public static int findTheAverage(int one, int two, int three) {
-        int max = Math.max(three, Math.max(one, two));
-        int min = Math.min(three, Math.min(one, two));
-        return one + two + three - max - min;
+        int max = Math.max(three, Math.max(one, two)); //find "max" number
+        int min = Math.min(three, Math.min(one, two)); //find "min" number
+        return one + two + three - max - min; // average number
     }
 }
