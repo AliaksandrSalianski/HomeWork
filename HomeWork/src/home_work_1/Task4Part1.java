e34265cb57814c7337674fdb0402ddbca0b74de5
AliaksandrSalianski <AliaksandrSalianski@gmail.com>
2022-07-12 15:58:47 -0400

    Task4Part1 new method Math.Random

--- HomeWork/src/home_work_1/Task4Part1.java
+++ HomeWork/src/home_work_1/Task4Part1.java
@@ -2,7 +2,14 @@ package home_work_1;
 
 
 public class Task4Part1 {
-    public static boolean defineOddNumber(int number) {
-        return (number % 2 == 0);
+
+    public static void defineOddNumber() {
+        int random = (int) (1 + Math.random() * 100); //random number from 1 to 99
+        int numberTwo = random + 1;
+        if (random % 2 == 0) {
+            System.out.printf("Not Even number is %d\n", numberTwo);
+        } else {
+            System.out.printf("Not Even number is %d\n", random);
+        }
     }
 }
