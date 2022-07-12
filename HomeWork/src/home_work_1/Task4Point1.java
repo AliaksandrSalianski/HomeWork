3b64b665f8fd8f3593d4344e7aa436ecb36e29cd
AliaksandrSalianski <AliaksandrSalianski@gmail.com>
2022-07-12 11:50:49 -0400

    TAsk4point1 OddNumber

--- HomeWork/src/home_work_1/Task4Point1.java
+++ HomeWork/src/home_work_1/Task4Point1.java
@@ -2,7 +2,7 @@ package home_work_1;
 
 
 public class Task4Point1 {
-    public static boolean oddNumber(int number) {
+    public static boolean defineOddNumber(int number) {
         return (number % 2 == 0);
     }
 }
