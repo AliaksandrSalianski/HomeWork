ead255b43f0e56756010d786b8ef8355148f25e0
AliaksandrSalianski <AliaksandrSalianski@gmail.com>
2022-07-13 15:48:10 -0400

    HomeWork/src/home_work_1/Task5.java

---
+++ HomeWork/src/home_work_1/Task5.java
@@ -0,0 +1,15 @@
+package home_work_1;
+
+public class Task5 {
+    public static void main(String[] args) {
+
+    }
+
+    public static void sleepIn(boolean weekDay, boolean vacation) {
+        if (vacation || !weekDay) {
+            System.out.println("Sleep");
+        } else {
+            System.out.println("Wake up!");
+        }
+    }
+}
