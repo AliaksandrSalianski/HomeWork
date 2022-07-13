c210dd0bd1476dfef0bb398df3c35c2ab57fc5f2
AliaksandrSalianski <AliaksandrSalianski@gmail.com>
2022-07-13 17:45:02 -0400

    Task6Part1

---
+++ HomeWork/src/home_work_1/Task6Part1.java
@@ -0,0 +1,26 @@
+package home_work_1;
+
+import java.util.Objects;
+import java.util.Scanner;
+
+public class Task6Part1 {
+    public static void main(String[] args) {
+        System.out.println("Enter your name");
+        Scanner scanner = new Scanner(System.in);
+        if (scanner.hasNextLine()) {
+            System.out.println("wrong input");
+            return;
+        }
+        String name = scanner.nextLine();
+        if (Objects.equals(name, "Вася")) {
+            System.out.println("Привет!");
+        }
+        if (Objects.equals(name, "Анастасия")) {
+            System.out.print("Я тебя так долго ждал");
+        }
+        if (!Objects.equals(name, "Вася") && !Objects.equals(name, "Анастасия")) {
+            System.out.println("Добрый день, а вы кто?");
+        }
+    }
+}
+
