9b08438bd61199c060a7e6dbf95b5542086ef8bd
AliaksandrSalianski <AliaksandrSalianski@gmail.com>
2022-07-14 11:42:59 -0400

    Task6Part1. hasNextLine not work,make nextLong || nextFloat

--- HomeWork/src/home_work_1/Task6Part1.java
+++ HomeWork/src/home_work_1/Task6Part1.java
@@ -7,20 +7,19 @@ public class Task6Part1 {
     public static void main(String[] args) {
         System.out.println("Enter your name");
         Scanner scanner = new Scanner(System.in);
-        if (scanner.hasNextLine()) {
+        if (scanner.hasNextLong() || scanner.hasNextFloat()) {
             System.out.println("wrong input");
-            return;
-        }
-        String name = scanner.nextLine();
-        if (Objects.equals(name, "Вася")) {
-            System.out.println("Привет!");
-        }
-        if (Objects.equals(name, "Анастасия")) {
-            System.out.print("Я тебя так долго ждал");
-        }
-        if (!Objects.equals(name, "Вася") && !Objects.equals(name, "Анастасия")) {
-            System.out.println("Добрый день, а вы кто?");
+        } else {
+            String name = scanner.nextLine();
+            if (Objects.equals(name, "Вася")) {
+                System.out.println("Привет!");
+            }
+            if (Objects.equals(name, "Анастасия")) {
+                System.out.print("Я тебя так долго ждал");
+            }
+            if (!Objects.equals(name, "Вася") && !Objects.equals(name, "Анастасия")) {
+                System.out.println("Добрый день, а вы кто?");
+            }
         }
     }
 }
-
