2b8e3ef65b1155bf6c6f09345fbb6a7e0ae2e664
AliaksandrSalianski <AliaksandrSalianski@gmail.com>
2022-07-18 19:02:51 -0400

    Loop3 v1.0

---
+++ HomeWork/src/homeWork_2/loops/Loop3.java
@@ -0,0 +1,45 @@
+package homeWork_2.loops;
+
+import java.util.Scanner;
+
+public class Loop3 {
+    /**
+     *  Возведение в степень. Через консоль пользователь вводит два числа. Первое число это число которое мы будем возводить,
+     *  Второе число это степень в которую возводят первое число.
+     *  Степень - только положительная и целая. Возводимое число - может быть отрицательным и оно будет дробным. Math использовать нельзя.
+     */
+
+    public static void main(String[] args) {
+        exponentiation();
+    }
+
+    public static void exponentiation() {
+        double result = 1;
+        double number = 0;
+        int degree = 0;
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("enter number");
+        if (scanner.hasNextDouble()) {
+            number = scanner.nextDouble();
+        } else {
+            System.out.println("wrong number");
+            return;
+        }
+        System.out.println("enter degree");
+        if (scanner.hasNextInt()) {
+            degree = scanner.nextInt();
+        } else {
+            System.out.println("wrong degree");
+            return;
+        }
+        if (degree < 0) {
+            System.out.println("degree < 0");
+            return;
+        } else {
+            for (int i = 1; i <= degree; i++) {
+                result = result * number;
+            }
+        }
+        System.out.println(number +" ^ "+ degree +" = "+result);
+    }
+}
