847c76f391d5f10adf5294d3859a29a4056d5c1a
AliaksandrSalianski <AliaksandrSalianski@gmail.com>
2022-07-19 17:25:17 -0400

    Array4 add method printTwoMinInArray

--- HomeWork/src/homeWork_2/arrays/Array4.java
+++ HomeWork/src/homeWork_2/arrays/Array4.java
@@ -8,13 +8,13 @@ public class Array4 {
      * 1. Сумма четных положительных элементов массива
      * 2. Максимальный из элементов массива с четными индексами
      * 3. Элементы массива, которые меньше среднего арифметического
-     * 2.4.4. Найти два наименьших (минимальных) элемента массива
-     * 2.4.5. Сжать массив, удалив элементы, принадлежащие интервалу
+     * 4. Найти два наименьших (минимальных) элемента массива
+     * 5. Сжать массив, удалив элементы, принадлежащие интервалу
      * 2.4.6. Сумма цифр массива
      */
     public static void main(String[] args) {
-        int[] array = ArraysUtils.arrayRandom(50, 100);
-       printItemsBelowAverage(array);
+        int[] array = ArraysUtils.arrayRandom(3, 100);
+        printTwoMinInArray(array);
     }
 
     //1
@@ -29,7 +29,7 @@ public class Array4 {
     }
 
     //2
-    public static int printMaxOfEven(int[] array) {
+    public static int getMaxOfEven(int[] array) {
         int numberMax = array[0];
         for (int i = 2; i < array.length; i += 2) {
             if (array[i] > numberMax) {
@@ -53,9 +53,27 @@ public class Array4 {
                 result.append(num).append(" ");
             }
         }
-        System.out.println("Average = "+ average);
+        System.out.println("Average = " + average);
         System.out.println("Items below average is : ");
         System.out.println(result);
     }
+
+    //4
+    public static void printTwoMinInArray(int[] array) {
+        int maxOne = Integer.MIN_VALUE;
+        int maxTwo = Integer.MIN_VALUE;
+        for (int nb : array) {
+            if (maxOne < nb) {
+                maxTwo = maxOne;
+                maxOne = nb;
+            } else if (maxOne > nb && maxTwo < nb) {
+                maxTwo = nb;
+            }
+        }
+        System.out.println("Max first = " + maxOne);
+        System.out.println("Max second = " + maxTwo);
+    }
+
+    //5
 
 }
