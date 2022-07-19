045f529a768455cba924da6f5cb679420d762c03
AliaksandrSalianski <AliaksandrSalianski@gmail.com>
2022-07-19 11:11:23 -0400

    Loop6 finish

--- HomeWork/src/homeWork_2/loops/Loop6.java
+++ HomeWork/src/homeWork_2/loops/Loop6.java
@@ -1,8 +1,23 @@
 package homeWork_2.loops;
 
-import java.security.SecureRandom;
-import java.util.Scanner;
-
 public class Loop6 {
+    /**
+     * выводит таблицу умножения
+     */
 
+    public static void displayMultiplicationTable() {
+        for (int i = 1; i <= 10; i++) {
+            for (int j = 2; j <= 5; j++) {
+                System.out.printf("%d * %d = %d\t", j, i, i * j);
+            }
+            System.out.println("");
+        }
+        System.out.println("...............................................");
+        for (int i = 1; i <= 10; i++) {
+            for (int j = 6; j <= 9; j++) {
+                System.out.printf("%d * %d = %d\t", j, i, i * j);
+            }
+            System.out.println("");
+        }
+    }
 }
