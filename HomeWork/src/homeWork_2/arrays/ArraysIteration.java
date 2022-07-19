2d371cb1081f3bbfe46532dfd7af5abac1dfb3c4
AliaksandrSalianski <AliaksandrSalianski@gmail.com>
2022-07-19 13:48:18 -0400

    ArrayIteration add method printReverse

--- HomeWork/src/homeWork_2/arrays/ArraysIteration.java
+++ HomeWork/src/homeWork_2/arrays/ArraysIteration.java
@@ -1,5 +1,7 @@
 package homeWork_2.arrays;
 
+import java.lang.reflect.Array;
+
 /**
  * Создать класс ArraysIteration. В этом клссе написать перебор массива при помощи do....while, while, for, foreach.
  * Вызывая ранее созданный метод arrayFromConsole (ArraysUtils.arrayFromConsole()) получить массив. Все задачи в одном классе, в отдельных методах. Должно получиться 3 метода:
@@ -8,12 +10,7 @@ package homeWork_2.arrays;
  * 3. Вывести все элементы массива в консоль в обратном порядке.
  */
 public class ArraysIteration {
-
-
-    public static void main(String[] args) {
-        printEverySecond();
-    }
-
+    //1
     public static void printAllElements() {
         int[] array = ArraysUtils.arrayFromConsole();
         //do while
@@ -41,34 +38,69 @@ public class ArraysIteration {
         }
     }
 
+    //2
     public static void printEverySecond() {
         int[] array = ArraysUtils.arrayFromConsole();
         //do while
         int i = 0;
         do {
-            System.out.print(array[i]);
+            System.out.print(array[i] + " ");
             i += 2;
         } while (i < array.length);
         System.out.println("");
         //while
         i = 0;
         while (i < array.length) {
-            System.out.print(array[i]);
+            System.out.print(array[i] + " ");
             i += 2;
         }
         System.out.println();
         //for
         for (int j = 0; j < array.length; j += 2) {
-            System.out.print(array[j]);
+            System.out.print(array[j] + " ");
         }
         //foreach
         System.out.println("");
         int k = 0;
         for (int n : array) {
             if (k % 2 == 0) {
-                System.out.print(n);
+                System.out.print(n + " ");
             }
             k += 1;
+        }
+    }
+
+    //3
+    public static void printReverse() {
+        int[] array = ArraysUtils.arrayFromConsole();
+        //do while
+        int i = array.length - 1;
+        do {
+            System.out.print(array[i] + " ");
+            i--;
+        } while (i >= 0);
+        System.out.println("");
+        //while
+        i = array.length - 1;
+        while (i >= 0) {
+            System.out.print(array[i] + " ");
+            i--;
+        }
+        System.out.println();
+        //for
+        for (int j = array.length - 1; j >= 0; j--) {
+            System.out.print(array[j] + " ");
+        }
+        System.out.println("");
+        //foreach
+        int[] arrayRevere = new int[array.length];
+        int a = array.length - 1;
+        for (int j = 0; j < arrayRevere.length; j++) {
+            arrayRevere[j] = array[a];
+            a--;
+        }
+        for (int n : arrayRevere) {
+            System.out.print(n + " ");
         }
     }
 }
