e718d00d39dd505aa2fc815a000a34df1953ada5
AliaksandrSalianski <AliaksandrSalianski@gmail.com>
2022-07-19 13:05:30 -0400

    ArraysIteration add method printEverySecond

--- HomeWork/src/homeWork_2/arrays/ArraysIteration.java
+++ HomeWork/src/homeWork_2/arrays/ArraysIteration.java
@@ -3,15 +3,15 @@ package homeWork_2.arrays;
 /**
  * Создать класс ArraysIteration. В этом клссе написать перебор массива при помощи do....while, while, for, foreach.
  * Вызывая ранее созданный метод arrayFromConsole (ArraysUtils.arrayFromConsole()) получить массив. Все задачи в одном классе, в отдельных методах. Должно получиться 3 метода:
- * 2.2.1. Вывести все элементы в консоль.
- * 2.2.2. Вывести каждый второй элемент массива в консоль.
- * 2.2.3. Вывести все элементы массива в консоль в обратном порядке.
+ * 1. Вывести все элементы в консоль.
+ * 2. Вывести каждый второй элемент массива в консоль.
+ * 3. Вывести все элементы массива в консоль в обратном порядке.
  */
 public class ArraysIteration {
 
 
     public static void main(String[] args) {
-        printAllElements();
+        printEverySecond();
     }
 
     public static void printAllElements() {
@@ -34,9 +34,41 @@ public class ArraysIteration {
         for (int j = 0; j < array.length; j++) {
             System.out.print(array[j]);
         }
+        //foreach
         System.out.println("");
         for (int n : array) {
             System.out.print(n);
+        }
+    }
+
+    public static void printEverySecond() {
+        int[] array = ArraysUtils.arrayFromConsole();
+        //do while
+        int i = 0;
+        do {
+            System.out.print(array[i]);
+            i += 2;
+        } while (i < array.length);
+        System.out.println("");
+        //while
+        i = 0;
+        while (i < array.length) {
+            System.out.print(array[i]);
+            i += 2;
+        }
+        System.out.println();
+        //for
+        for (int j = 0; j < array.length; j += 2) {
+            System.out.print(array[j]);
+        }
+        //foreach
+        System.out.println("");
+        int k = 0;
+        for (int n : array) {
+            if (k % 2 == 0) {
+                System.out.print(n);
+            }
+            k += 1;
         }
     }
 }
