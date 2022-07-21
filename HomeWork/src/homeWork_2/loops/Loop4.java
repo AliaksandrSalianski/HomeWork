312d2f72f7167ab196768a3d61c4a1c6a9fcbccc
AliaksandrSalianski <AliaksandrSalianski@gmail.com>
2022-07-21 10:21:19 -0400

    Loop4 finish clean cod

--- HomeWork/src/homeWork_2/loops/Loop4.java
+++ HomeWork/src/homeWork_2/loops/Loop4.java
@@ -1,24 +1,28 @@
 package homeWork_2.loops;
 
+import java.util.Random;
+
 public class Loop4 {
     /**
-     * Метод умножает число long a = 1; пока не произойдёт переполнение. В конце в консоль выводится сообщение со значением до переполнения и после переполнения. Умножается на интервал от
-     * [-Integer.Min_Value;Integer.Max_Value] путем добавления рандомно знака "-".
+     * Метод умножает число long a = 1 на рандомное число пока не произойдёт переполнение.
+     * В конце в консоль выводится сообщение со значением до переполнения и после переполнения.
      */
+    public static void main(String[] args) {
+        multiplyToOverflow();
+    }
     public static void multiplyToOverflow() {
         long a = 1;
         long previousValueA = 1;
+        Random random = new Random();
         while (true) {
-            int sing = Math.random() < 0.5 ? -1 : 1;
-            int random = (int) (Math.random() * Integer.MAX_VALUE) * sing;
-            random = random == -Integer.MAX_VALUE ? random - 1 : random;
-            a *= random;
-            if ((previousValueA > 0 && random > 0 || previousValueA < 0 && random < 0) && a < 0) {
+            int randomInt = random.nextInt() ;
+            a *= randomInt;
+            if ((previousValueA > 0 && randomInt > 0 || previousValueA < 0 && randomInt < 0) && a < 0) {
                 System.out.println("overflow");
                 System.out.println("before overflow =" + previousValueA);
                 System.out.println("after overflow =" + a);
                 break;
-            } else if ((previousValueA < 0 && random > 0 || previousValueA > 0 && random < 0) && a > 0) {
+            } else if ((previousValueA < 0 && randomInt > 0 || previousValueA > 0 && randomInt < 0) && a > 0) {
                 System.out.println("overflow");
                 System.out.println("before overflow =" + previousValueA);
                 System.out.println("after overflow =" + a);
