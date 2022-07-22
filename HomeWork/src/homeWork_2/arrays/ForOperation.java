681bf3c62150edae317d887f8f79474c157514b9
AliaksandrSalianski <AliaksandrSalianski@gmail.com>
2022-07-22 16:19:10 -0400

    add ForOperation

--- HomeWork/src/homeWork_2/arrays/ForOperation.java
+++ HomeWork/src/homeWork_2/arrays/ForOperation.java
@@ -1,18 +1,24 @@
 package homeWork_2.arrays;
 
-public class ForOperation implements IArrayOperation{
+public class ForOperation implements IArrayOperation {
     @Override
     public void printAllElements(int[] array) {
-
+        for (int j = 0; j < array.length; j++) {
+            System.out.print(array[j]);
+        }
     }
 
     @Override
     public void printEverySecond(int[] array) {
-
+        for (int j = 0; j < array.length; j += 2) {
+            System.out.print(array[j] + " ");
+        }
     }
 
     @Override
     public void printReverse(int[] array) {
-
+        for (int j = array.length - 1; j >= 0; j--) {
+            System.out.print(array[j] + " ");
+        }
     }
 }
