d3a397b1c825951ec3e718ba76a99009a197b1ee
AliaksandrSalianski <AliaksandrSalianski@gmail.com>
2022-07-22 10:13:27 -0400

    Loop1 final version

--- HomeWork/src/homeWork_2/loops/Loop1.java
+++ HomeWork/src/homeWork_2/loops/Loop1.java
@@ -6,6 +6,9 @@ public class Loop1 {
      * в случае переполнения выводится сообщение со значением до переполнения и при переполненнии
      * Используется только цикл
      */
+    public static void main(String[] args) {
+        System.out.println(multiplyWithCycle(27));
+    }
 
     public static String multiplyWithCycle(int number) {
         StringBuilder str = new StringBuilder();
@@ -17,9 +20,9 @@ public class Loop1 {
             for (int i = 1; i <= number; i++) {
                 previousResult *= i;
                 if (previousResult < 0) {
-                    System.out.println(i + " overflow of \"long\" type number= " + "result =" + previousResult);
-                    System.out.printf("value at i = %d = %d", i - 1, result);
-                    break;
+                    System.out.println("With a value " + i + " overflow of \"long\" type = " + previousResult);
+                    System.out.printf("Value before overflow at number %d = %d\n", i - 1, result);
+                    return str+"\b = " + result;
                 } else {
                     result *= i;
                     String multiplyOrEquals = i == number ? "=" + result : "*";
@@ -30,38 +33,38 @@ public class Loop1 {
         return str.toString();
     }
 
- //  /**
- //   * Число положительное, максимум long:
- //   * Перемножить числа от 1 до числа (включительно) введенного через аргумент к исполняемой программе.
- //   * Используя рекурсию
- //   */
- //  public static long multiplyWithRecursion(int number) {
- //      // печатает в обратном порядке
- //      long result = 1;
- //      if (number < 0) {
- //          System.out.print("wrong input");
- //      }
- //      if (number == 1) {
- //          System.out.print("*1 = " + result);
- //          return result;
- //      }
- //      System.out.print(number);
- //      if (number != 2) {
- //          System.out.print("*");
- //      }
- //      result = number * multiplyWithRecursion(number - 1);
- //      if (result < 0) {
- //          System.out.println("");
- //          System.out.print(" overflow");
- //          System.out.println(" in number =" + number);
- //          return result;
- //      }
- //      return result;
- //  }
+    //  /**
+    //   * Число положительное, максимум long:
+    //   * Перемножить числа от 1 до числа (включительно) введенного через аргумент к исполняемой программе.
+    //   * Используя рекурсию
+    //   */
+    //  public static long multiplyWithRecursion(int number) {
+    //      // печатает в обратном порядке
+    //      long result = 1;
+    //      if (number < 0) {
+    //          System.out.print("wrong input");
+    //      }
+    //      if (number == 1) {
+    //          System.out.print("*1 = " + result);
+    //          return result;
+    //      }
+    //      System.out.print(number);
+    //      if (number != 2) {
+    //          System.out.print("*");
+    //      }
+    //      result = number * multiplyWithRecursion(number - 1);
+    //      if (result < 0) {
+    //          System.out.println("");
+    //          System.out.print(" overflow");
+    //          System.out.println(" in number =" + number);
+    //          return result;
+    //      }
+    //      return result;
+    //  }
 
- //  public static void main(String[] args) {
- //      System.out.println(multiplyWithCycle(10));
- //  }
+    //  public static void main(String[] args) {
+    //      System.out.println(multiplyWithCycle(10));
+    //  }
 
 }
 
