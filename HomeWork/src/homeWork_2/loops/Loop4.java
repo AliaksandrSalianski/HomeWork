841a2d767fbee6f49996c34102dbb16d014b1d20
AliaksandrSalianski <AliaksandrSalianski@gmail.com>
2022-07-22 11:01:56 -0400

    Loop 4 final version

--- HomeWork/src/homeWork_2/loops/Loop4.java
+++ HomeWork/src/homeWork_2/loops/Loop4.java
@@ -7,28 +7,22 @@ public class Loop4 {
      * Метод умножает число long a = 1 на рандомное число пока не произойдёт переполнение.
      * В конце в консоль выводится сообщение со значением до переполнения и после переполнения.
      */
-    public static void main(String[] args) {
-        multiplyToOverflow();
-    }
-    public static void multiplyToOverflow() {
+    public static String multiplyToOverflow() {
+        String strResult = "";
         long a = 1;
         long previousValueA = 1;
         Random random = new Random();
         while (true) {
-            int randomInt = random.nextInt() ;
+            int randomInt = random.nextInt();
             a *= randomInt;
+            strResult = "overflow\n \tbefore overflow = " + previousValueA + "\n\tafter overflow = " + a;
             if ((previousValueA > 0 && randomInt > 0 || previousValueA < 0 && randomInt < 0) && a < 0) {
-                System.out.println("overflow");
-                System.out.println("before overflow =" + previousValueA);
-                System.out.println("after overflow =" + a);
-                break;
+                return strResult;
             } else if ((previousValueA < 0 && randomInt > 0 || previousValueA > 0 && randomInt < 0) && a > 0) {
-                System.out.println("overflow");
-                System.out.println("before overflow =" + previousValueA);
-                System.out.println("after overflow =" + a);
-                break;
+                return strResult;
             }
             previousValueA = a;
         }
     }
+
 }
