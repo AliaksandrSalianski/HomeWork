14c3627eacb165aeafc3dba59c93fc64381f4784
AliaksandrSalianski <AliaksandrSalianski@gmail.com>
2022-07-22 09:48:41 -0400

    Loop2 make not void

--- HomeWork/src/homeWork_2/loops/Loop2.java
+++ HomeWork/src/homeWork_2/loops/Loop2.java
@@ -2,13 +2,19 @@ package homeWork_2.loops;
 
 import java.util.Scanner;
 
-/** Метод перемножает все числа введенного через аргумент числа и выводит ход вычислений в консоль.Число представляется как массив char при
- *  помощи цикла идет перебор всех элементов и получение значения из char.Выбор знака "*" или "=" при помощи тернарного опператора и индекса ячейки массива.
- *  Пользователь обязан ввести целое число. Если ввели не целое то выдается сообщение о том что пользователь ввёл некорректные данные.
+/**
+ * Метод перемножает все числа введенного через аргумент числа и выводит ход вычислений в консоль.Число представляется как массив char при
+ * помощи цикла идет перебор всех элементов и получение значения из char.Выбор знака "*" или "=" при помощи тернарного опператора и индекса ячейки массива.
+ * Пользователь обязан ввести целое число. Если ввели не целое то выдается сообщение о том что пользователь ввёл некорректные данные.
  */
 
 public class Loop2 {
-    public static void printMultiplication() {
+    public static void main(String[] args) {
+        System.out.println(printMultiplication());
+    }
+
+    public static String printMultiplication() {
+        String strResult = "";
         int number;
         int result = 1;
         System.out.println("Please, enter integer number ");
@@ -16,11 +22,9 @@ public class Loop2 {
         if (scanner.hasNextInt()) {
             number = scanner.nextInt();
         } else if (scanner.hasNextDouble()) {
-            System.out.println("Введено не целое число");
-            return;
+            return "Введено не целое число";
         } else {
-            System.out.println("Введено не число");
-            return;
+            return "Введено не число";
         }
         String str = "" + number;
         char[] charArray = str.toCharArray();
@@ -28,9 +32,10 @@ public class Loop2 {
             int charToNumber = Character.digit(charArray[i], 10);
             result *= charToNumber;
             String multiplyOrEquals = i == charArray.length - 1 ? "=" + result : "*";
-            System.out.print(charArray[i] + multiplyOrEquals);
+            strResult += charArray[i] + multiplyOrEquals;
         }
         scanner.close();
+        return strResult;
     }
 }
 
