bb7f8520eaf33e653dd37362a676e4b67a547799
AliaksandrSalianski <AliaksandrSalianski@gmail.com>
2022-07-22 18:57:41 -0400

    add buble SortUtils

---
+++ HomeWork/src/homeWork_2/utils/SortUtils.java
@@ -0,0 +1,22 @@
+package homeWork_2.utils;
+
+import java.util.Arrays;
+
+public class SortUtils {
+    public static void bubble(int[] arr) {
+        for (int i = 0; i < arr.length; i++) {
+            for (int j = (arr.length-1); j > i; j--)
+            {
+                if (arr[j - 1] > arr[j])
+                {
+                    int helper = arr[j - 1];
+                    arr[j - 1] = arr[j];
+                    arr[j] = helper;
+                }
+            }
+        }
+        System.out.println(Arrays.toString(arr));
+    }
+
+}
+
