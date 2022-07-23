83b5c8f15c6823480caeb7770522d3573f4d0825
AliaksandrSalianski <AliaksandrSalianski@gmail.com>
2022-07-22 22:13:34 -0400

    SortUtils only buble finish

--- HomeWork/src/homeWork_2/utils/SortUtils.java
+++ HomeWork/src/homeWork_2/utils/SortUtils.java
@@ -2,13 +2,14 @@ package homeWork_2.utils;
 
 import java.util.Arrays;
 
+/**
+ * Сортировка массива метотом "Пузырька"
+ */
 public class SortUtils {
     public static void bubble(int[] arr) {
         for (int i = 0; i < arr.length; i++) {
-            for (int j = (arr.length-1); j > i; j--)
-            {
-                if (arr[j - 1] > arr[j])
-                {
+            for (int j = (arr.length - 1); j > i; j--) {
+                if (arr[j - 1] > arr[j]) {
                     int helper = arr[j - 1];
                     arr[j - 1] = arr[j];
                     arr[j] = helper;
