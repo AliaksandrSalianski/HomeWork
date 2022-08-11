112a3f20633b124e11c2df75420fb29e4694b523
AliaksandrSalianski <AliaksandrSalianski@gmail.com>
2022-08-11 16:19:08 -0400

    DataComparatorHash

---
+++ HomeWork/src/homeWork_4/DataComparatorHash.java
@@ -0,0 +1,24 @@
+package homeWork_4;
+
+import java.util.Comparator;
+
+
+public class DataComparatorHash implements Comparator<Object> {
+
+
+    @Override
+    public int compare(Object a, Object b) {
+        if(a == null){
+            return 1;
+        }else
+        if (b != null && a.hashCode() > b.hashCode()) {
+            return 1;
+        } else if (b != null && a.hashCode() < b.hashCode()) {
+            return -1;
+        } else {
+            return 0;
+        }
+    }
+
+}
+
