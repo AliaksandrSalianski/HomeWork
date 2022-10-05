785804abf00b5524e5482cf9696279b3b063159a
AliaksandrSalianski <AliaksandrSalianski@gmail.com>
2022-10-05 15:01:54 -0400

    Task7 v 2.1

---
+++ HomeWork/src/task7and8/runner/MainTask7Version2.java
@@ -0,0 +1,39 @@
+package task7and8.runner;
+
+import task7and8.api.ILocalFileService;
+import task7and8.dto.LocalStorageFileService;
+
+import java.nio.file.Path;
+import java.util.List;
+import java.util.Objects;
+
+/**
+ * Класс реализует работу с текстовыми файлами находящиеся в папке которую указал пользователь. В текстовом файле  ищется количество употребления вводимого  слова и результат
+ * поиска сохраняется в папку указанную пользователем
+ */
+public class MainTask7Version2 {
+    public static void main(String[] args) {
+        ILocalFileService worker = new LocalStorageFileService();
+        System.out.println("Введите адрес папки");
+        String enteredPathByUser = worker.makeStringWithBuffer();
+        List<Path> filePaths = worker.getAllFiles(enteredPathByUser);
+        System.out.println("Все \"txt\" файлы в выбранной папке:");
+        worker.printAllTxtFileNameInFolder(filePaths);
+        System.out.println("Укажите файл для последующей работы");
+        String selectedFileToWork = worker.makeStringWithBuffer();
+        Path foundedFile = worker.findFileInListPath(filePaths, selectedFileToWork);
+        if (foundedFile == null) return;
+        System.out.println("Файл найден");
+        String text = worker.getText(foundedFile);
+        while (true) {
+            System.out.println("Введите \"Слово_для_поиска\" или \"Esc\" для выхода из программы");
+            String wordToFind = worker.makeStringWithBuffer();
+            if (Objects.equals(wordToFind, "Esc")) {
+                break;
+            } else {
+                worker.findWithISearchEngineAndPutResultInFolder(worker.getResultFolderPath(), selectedFileToWork, text, wordToFind);
+            }
+        }
+        System.out.println("exit");
+    }
+}
